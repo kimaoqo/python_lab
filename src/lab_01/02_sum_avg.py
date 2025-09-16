a61dd71118781f955aa8d462d6035b70e56375a6
kimaoqo <newlasapol1999@mail.ru>
2025-09-16 10:02:23 +0300

    1

--- src/lab_01/02_sum_avg.py
+++ src/lab_01/02_sum_avg.py
@@ -2,4 +2,4 @@ a = float(input('a: ').replace(',','.'))
 b = float(input('b: ').replace(',','.'))
 summ = a + b
 avg = summ / 2
-print('sum=', round(summ, 2), '; avg =',round(avg, 2))
+print(f'sum={summ: .2f}; avg={avg: .2f}')
